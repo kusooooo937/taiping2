993fc4e7601517bed3f7b271b7d125d1eb9ceb30
kusooooo937 <kiritotianzhong3@gmail.com>
2025-09-04 17:33:08 +0900

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 // 単語リスト
-const words = ["apple", "banana", "cherry", "orange", "grape", "melon"];
+const words = ["saito","uehasi","jimura","uno","hayakawa","fujii","maegawa","usui","yamamoto","higuti","ruuku","igawa","fumuro","matui","ootani","kawaguti","akane","sugai];
 let currentWord = "";
 let currentIndex = 0;
 let score = 0;
@@ -142,6 +142,7 @@ function gameLoop() {
 
 // ループ開始
 gameLoop();
+
 
 
 
